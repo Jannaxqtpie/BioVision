952c29a642336c2233d59153e5221be57348f6c8
Jannaxqtpie <162796813+Jannaxqtpie@users.noreply.github.com>
2025-11-18 20:43:41 +0800

    Add files via upload

---
+++ Java/gallery/home/slideshow/SlideshowFragment.kt
@@ -0,0 +1,50 @@
+package com.surendramaran.yolov8tflite.ui.slideshow
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.surendramaran.yolov8tflite.DrawerNav
+import com.surendramaran.yolov8tflite.R
+import com.surendramaran.yolov8tflite.databinding.FragmentSlideshowBinding
+
+class SlideshowFragment : Fragment() {
+
+    private var _binding: FragmentSlideshowBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val slideshowViewModel =
+            ViewModelProvider(this).get(SlideshowViewModel::class.java)
+
+        _binding = FragmentSlideshowBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+
+        val buttonView: Button = root.findViewById(R.id.guideback)
+
+        buttonView.setOnClickListener {
+            val intent = Intent(requireContext(), DrawerNav::class.java)
+            startActivity(intent)
+        }
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
